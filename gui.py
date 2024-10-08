374ac854ae5c7742a5f12925a406f8c1003ba425
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-10-08 16:08:15 +1030

    Replace Combobox with more appropriate Listbox

--- gui.py
+++ gui.py
@@ -26,18 +26,32 @@ class HAMApp:
             self.tree.column(col, stretch=tk.YES, width=100)
         self.tree.pack(fill=tk.BOTH, expand=True)
 
-        # Create column selection
-        self.column_selection = tk.StringVar(value="Select columns")
-        self.column_dropdown = ttk.Combobox(root, textvariable=self.column_selection, values=self.columns)
-        self.column_dropdown.pack(side=tk.LEFT, padx=10)
-        self.column_dropdown.bind("<<ComboboxSelected>>", self.toggle_column)
+        # # Create column selection
+        # self.column_selection = tk.StringVar(value="Select columns")
+        # self.column_dropdown = ttk.Combobox(root, textvariable=self.column_selection, values=self.columns)
+        # self.column_dropdown.pack(side=tk.LEFT, padx=10)
+        # self.column_dropdown.bind("<<ComboboxSelected>>", self.toggle_column)
 
-    def toggle_column(self, event):
-        selected_column = self.column_selection.get()
-        if selected_column in self.visible_cols:
-            self.visible_cols.remove(selected_column)
-        else:
-            self.visible_cols.append(selected_column)
+        # Create columns selection listbox
+        self.column_selection_frame = tk.Frame(root)
+        self.column_selection_frame.pack(side=tk.LEFT, padx=10)
+
+        self.column_listbox = tk.Listbox(self.column_selection_frame, selectmode=tk.MULTIPLE)
+        for col in self.columns:
+            self.column_listbox.insert(tk.END, col)
+
+        # Pre-select already visible columns
+        for index, col in enumerate(self.columns):
+            if col in self.visible_cols:
+                self.column_listbox.select_set(index)
+
+        self.column_listbox.pack()
+
+        self.column_listbox.bind("<<ListboxSelect>>", self.update_visible_cols)
+
+    def update_visible_cols(self, event):
+        selected_indices = self.column_listbox.curselection()
+        self.visible_cols = [self.column_listbox.get(i) for i in selected_indices]
         self.update_column_visibility()
 
     def update_column_visibility(self):
