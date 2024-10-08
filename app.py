83317f67537ad8549ba1229613042d5d3f074397
jameson-dev <178156579+jameson-dev@users.noreply.github.com>
2024-10-08 15:59:06 +1030

    Implement Tk GUI to app

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
+import tkinter as tk
+
 import sqlite
+import gui
 
 
 def main():
     sqlite.load_db()
     sqlite.fetch_data()
+    root = tk.Tk()
+    app = gui.HAMApp(root)
+    root.mainloop()
 
 
 if __name__ == "__main__":
     main()
-
-
-
